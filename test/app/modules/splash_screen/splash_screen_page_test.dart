cc571e8f81eb7623ed28279a9c9f5fc5bf9b7f3a
renankanu <renankanu@gmail.com>
2020-11-29 16:15:41 -0300

    Refactor - cleaning dirt

--- test/app/modules/splash_screen/splash_screen_page_test.dart
+++ test/app/modules/splash_screen/splash_screen_page_test.dart
@@ -1,8 +1,4 @@
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:flutter_modular/flutter_modular_test.dart';
-
-import 'package:app_kapets/app/modules/splash_screen/splash_screen_page.dart';
 
 void main() {
   testWidgets('SplashScreenPage has title', (tester) async {
