b63567a23b42493da6426f7dfdfd97faf59eb2cc
renankanu <renankanu@gmail.com>
2020-11-29 16:37:11 -0300

    Feature - create routes files and navigate after 5 secounds to home screen

--- lib/app/modules/splash_screen/splash_screen_page.dart
+++ lib/app/modules/splash_screen/splash_screen_page.dart
@@ -1,5 +1,9 @@
+import 'dart:async';
+
+import 'package:app_kapets/app/routes/routes.dart';
 import 'package:app_kapets/utils/CustomColors.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_svg/svg.dart';
 
 class SplashScreenPage extends StatefulWidget {
@@ -9,42 +13,55 @@ class SplashScreenPage extends StatefulWidget {
 
 class _SplashScreenPageState extends State<SplashScreenPage> {
   @override
+  void initState() {
+    super.initState();
+    Timer(Duration(seconds: 5), navigationPage);
+  }
+
+  void navigationPage() {
+    Navigator.pushNamed(context, Routes.home);
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: CustomColors.revolver,
-      body: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Text(
-              'Kapets',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                color: CustomColors.rajah,
-                fontSize: 32,
-                fontWeight: FontWeight.bold,
-                letterSpacing: 12,
+    return AnnotatedRegion<SystemUiOverlayStyle>(
+      value: SystemUiOverlayStyle.light,
+      child: Scaffold(
+        backgroundColor: CustomColors.revolver,
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Text(
+                'Kapets',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: CustomColors.rajah,
+                  fontSize: 32,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 12,
+                ),
               ),
-            ),
-            Text(
-              'faça um novo amigo',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                color: CustomColors.rajah.withOpacity(.5),
-                fontSize: 16,
+              Text(
+                'faça um novo amigo',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: CustomColors.rajah.withOpacity(.5),
+                  fontSize: 16,
+                ),
               ),
-            ),
-            SizedBox(height: 40),
-            Container(
-              width: 100,
-              height: 100,
-              child: SvgPicture.asset(
-                'assets/images/logo.svg',
-                color: CustomColors.rajah,
-              ),
-            )
-          ],
+              SizedBox(height: 40),
+              Container(
+                width: 100,
+                height: 100,
+                child: SvgPicture.asset(
+                  'assets/images/logo.svg',
+                  color: CustomColors.rajah,
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
